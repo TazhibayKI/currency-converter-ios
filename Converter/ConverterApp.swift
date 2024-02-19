4b0e236db5c921c7e309274cfb59a84df14af586
TazhibayKI <tazhibayki@mail.ru>
2024-02-19 17:03:25 +0600

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,32 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Александра Тажибай on 19.02.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ConverterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
